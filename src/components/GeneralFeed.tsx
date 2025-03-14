05e7c5ad472b889036fadd19373cc9caf162b270
MoncefDrew <moncef.mokrani@etu.univ-batna2.dz>
2025-03-14 17:42:30 +0100

    adding suggested posts for new users

--- src/components/GeneralFeed.tsx
+++ src/components/GeneralFeed.tsx
@@ -1,11 +1,9 @@
-import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 import PostFeed from "./PostFeed"
 import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config"
 
 const GeneralFeed = async () => {
     
-    const session = getAuthSession()
     const posts = await db.post.findMany({
         
         orderBy:{
