9a2639242e11cfaee7c8f9ab91f9b58c020a0d32
MoncefDrew <moncef.mokrani@etu.univ-batna2.dz>
2025-03-14 01:07:15 +0100

    fixing some postfeed errors

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -23,72 +23,70 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
   })
   const { data: session } = useSession()
 
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery(
     ['infinite-query'],
     async ({ pageParam = 1 }) => {
       const query =
         `/api/posts?limit=${INFINITE_SCROLL_PAGINATION_RESULTS}&page=${pageParam}` +
-        (!!subredditName ? `&subredditName=${subredditName}` : '')
+        (subredditName ? `&subredditName=${subredditName}` : '')
 
       const { data } = await axios.get(query)
       return data as ExtendedPost[]
     },
-
     {
-      getNextPageParam: (_, pages) => {
-        return pages.length + 1
-      },
+      getNextPageParam: (lastPage, pages) => (lastPage.length ? pages.length + 1 : undefined),
       initialData: { pages: [initialPosts], pageParams: [1] },
     }
   )
 
   useEffect(() => {
-    if (entry?.isIntersecting) {
-      fetchNextPage() // Load more posts when the last post comes into view
+    if (entry?.isIntersecting && !isFetchingNextPage && hasNextPage) {
+      fetchNextPage()
     }
-  }, [entry, fetchNextPage])
+  }, [entry, fetchNextPage, isFetchingNextPage, hasNextPage])
 
-  const posts = data?.pages.flatMap((page) => page) ?? initialPosts
+  const posts = data?.pages?.flatMap((page) => page) ?? []
 
   return (
     <ul className='flex flex-col col-span-2 space-y-6'>
-      {posts.map((post, index) => {
-        const votesAmt = post.votes.reduce((acc, vote) => {
-          if (vote.type === 'UP') return acc + 1
-          if (vote.type === 'DOWN') return acc - 1
-          return acc
-        }, 0)
+      {posts.length > 0 ? (
+        posts.map((post, index) => {
+          const votesAmt = post.votes.reduce((acc, vote) => {
+            if (vote.type === 'UP') return acc + 1
+            if (vote.type === 'DOWN') return acc - 1
+            return acc
+          }, 0)
 
-        const currentVote = post.votes.find(
-          (vote) => vote.userId === session?.user.id
-        )
+          const currentVote = post.votes.find((vote) => vote.userId === session?.user.id)
 
-        if (index === posts.length - 1) {
-          // Add a ref to the last post in the list
-          return (
-            <li key={post.id} ref={ref}>
+          if (index === posts.length - 1) {
+            return (
+              <li key={post.id} ref={ref}>
+                <Post
+                  post={post}
+                  commentAmt={post.comments.length}
+                  subredditName={post.subreddit.name}
+                  votesAmt={votesAmt}
+                  currentVote={currentVote}
+                />
+              </li>
+            )
+          } else {
+            return (
               <Post
+                key={post.id}
                 post={post}
                 commentAmt={post.comments.length}
                 subredditName={post.subreddit.name}
                 votesAmt={votesAmt}
                 currentVote={currentVote}
               />
-            </li>
-          )
-        } else {
-          return (
-            <Post
-              key={post.id}
-              post={post}
-              commentAmt={post.comments.length}
-              subredditName={post.subreddit.name}
-              votesAmt={votesAmt}
-              currentVote={currentVote}
-            />
-          )
-        }
-      })}
+            )
+          }
+        })
+      ) : (
+        <p className="text-center text-gray-500">No posts available.</p>
+      )}
 
       {isFetchingNextPage && (
         <li className='flex justify-center'>
