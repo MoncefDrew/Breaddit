80dc0e86c74ca4f6f7e7f46e8f583056f7278f17
Moncef Mokrani <moncef.mokrani@etu.univ-batna2.dz>
2025-04-08 11:27:01 +0100

    Add files via upload

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -1,52 +1,33 @@
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
-import { SubredditValidator } from '@/lib/validators/subreddit'
-import { z } from 'zod'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  try {
-    const session = await getAuthSession()
+  const session = await getAuthSession()
 
-    if (!session?.user) {
-      return new Response('Unauthorized', { status: 401 })
-    }
+  if (!session) return new NextResponse('Unauthorized', { status: 401 })
 
-    const body = await req.json()
-    const { name } = SubredditValidator.parse(body)
+  const { subredditId } = await req.json()
 
-    // check if subreddit already exists
-    const subredditExists = await db.subreddit.findFirst({
-      where: {
-        name,
-      },
-    })
+  if (!subredditId) return new NextResponse('Missing subreddit ID', { status: 400 })
 
-    if (subredditExists) {
-      return new Response('Subreddit already exists', { status: 409 })
-    }
+  const subreddit = await db.subreddit.findUnique({
+    where: { id: subredditId },
+  })
 
-    // create subreddit and associate it with the user
-    const subreddit = await db.subreddit.create({
-      data: {
-        name,
-        creatorId: session.user.id,
-      },
-    })
+  if (!subreddit) return new NextResponse('Subreddit not found', { status: 404 })
 
-    // creator also has to be subscribed
-    await db.subscription.create({
-      data: {
-        userId: session.user.id,
-        subredditId: subreddit.id,
-      },
-    })
+  const subscription = await db.subscription.findFirst({
+    where: {
+      subredditId,
+      userId: session.user.id,
+    },
+  })
 
-    return new Response(subreddit.name)
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return new Response(error.message, { status: 422 })
-    }
+  const isCreator = subreddit.creatorId === session.user.id
 
-    return new Response('Could not create subreddit', { status: 500 })
-  }
+  return NextResponse.json({
+    isJoined: !!subscription,
+    isCreator,
+  })
 }
